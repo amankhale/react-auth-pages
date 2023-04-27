49f6aa8d3762513e4c673b13fb6eb13acdc0e9c0
amankhale <akhale877@gmail.com>
2023-04-27 17:56:59 +0530

    Completed: forgot password

--- src/auth/pages/forgot-password/ForgotPassword.jsx
+++ src/auth/pages/forgot-password/ForgotPassword.jsx
@@ -1,15 +1,41 @@
+import { useState } from 'react';
 import './ForgotPassword.css';
 import { Link } from 'react-router-dom';
+import EmailInput from '../../../inputFields/InputEmail';
 
 export default function ForgotPassword() {
+
+    const [errors, setErrors] = useState({});
+    const [email, setEmail] = useState('');
+    const [isFormSubmitted, setSubmit] = useState(false);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        setSubmit(true);
+        let isError = Object.values(errors).filter(x => x)?.length;
+        if (!isError) {
+            let payload = { email };
+            console.log(payload);
+        }
+    }
+
     return (
         <div className="container">
             <input type="checkbox" id="check" />
             <div className="login form">
                 <header>Forgot Password</header>
-                <form action="#">
-                    <input type="text" placeholder="Enter your email" />
-                    <input type="button" className="button" defaultValue="Send Link" />
+                <form onSubmit={handleSubmit}>
+                    <div className="form-field">
+                        <EmailInput
+                            email={email}
+                            setEmail={setEmail}
+                            placeholder={"Enter your registered email"}
+                            isFormSubmitted={isFormSubmitted}
+                            errors={errors}
+                            setErrors={setErrors}>
+                        </EmailInput>
+                    </div>
+                    <input type="submit" className="button" defaultValue="Send Link" />
                 </form>
                 <div className="signup">
                     <span className="signup">
