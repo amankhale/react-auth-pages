df41479e129d1434c1fa677232623069869960f5
amankhale <akhale877@gmail.com>
2023-04-27 17:51:21 +0530

    Completed: Login form with validatoins

--- src/auth/pages/login/Login.jsx
+++ src/auth/pages/login/Login.jsx
@@ -1,15 +1,71 @@
 import './Login.css';
 import { Link } from 'react-router-dom';
+import EmailInput from '../../../inputFields/InputEmail';
+import { useEffect, useState } from 'react';
+import Validators from '../../../utility/Validators';
+import ErrorMessage from '../../../utility/ErrorMessage';
 
 export default function Login() {
+
+    const validators = new Validators();
+    const errorMessage = new ErrorMessage();
+
+    const [errors, setErrors] = useState({});
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isFormSubmitted, setSubmit] = useState(false);
+
+    useEffect(() => {
+        validatePassword();
+    }, [password]);
+
+    function validatePassword() {
+        if (validators.required(password)) {
+            setErrors((prevError) => {
+                return { ...prevError, password: 'required' };
+            });
+        } else {
+            setErrors((prevError) => {
+                return { ...prevError, password: null };
+            });
+        }
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        setSubmit(true);
+        let isError = Object.values(errors).filter(x => x)?.length;
+        if (!isError) {
+            let payload = { email, password };
+            console.log(payload);
+        }
+    }
+
+
     return (
         <div className="container">
             <input type="checkbox" id="check" />
             <div className="login form">
                 <header>Login</header>
-                <form>
-                    <input type="email" placeholder="Enter your email" required />
-                    <input type="password" placeholder="Enter your password" required />
+                <form onSubmit={handleSubmit}>
+                    <div className="form-field">
+                        <EmailInput
+                            email={email}
+                            isFormSubmitted={isFormSubmitted}
+                            placeholder={"Enter your email"}
+                            setEmail={setEmail}
+                            errors={errors}
+                            setErrors={setErrors}>
+                        </EmailInput>
+                    </div>
+                    <div className="form-field">
+                        <input className={(errors?.password && isFormSubmitted) ? "input-invalid" : ""} value={password} onInput={(e) => setPassword(e.target.value)} type="password" placeholder="Enter your password" />
+                        {
+                            isFormSubmitted && <div className="error-msg">
+                                {errors?.password == 'required' && errorMessage.required('Password')}
+                            </div>
+                        }
+                    </div>
                     <Link to={'/auth/forgot-password'}>Forgot password?</Link>
                     <input type="submit" className="button" defaultValue="Login" />
                 </form>
