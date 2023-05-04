aabe28f800295c60caa5a611e3bc0cf0d867138f
amankhale <akhale877@gmail.com>
2023-05-04 12:20:36 +0530

    Some changes done

--- src/Employee/slice/employee.slice.tsx
+++ src/Employee/slice/employee.slice.tsx
@@ -8,29 +8,32 @@ export const employeeSlice = createSlice({
     editEmployeeData: {},
   },
   reducers: {
-    deleteEmployee: (state, action: { payload: number; type: string }) => {
-      state.empList.splice(action.payload, 1);
-    },
     addEmployee: (state, action: { payload: Employee; type: string }) => {
       const data: Employee = action.payload;
-      
+
       const index = state.empList.findIndex((x: Employee) => x.id == data.id);
-      
+
       if (index == -1) {
         state.empList.push(data);
       } else {
         state.empList.splice(index, 1, data);
       }
     },
+
+    deleteEmployee: (state, action: { payload: number; type: string }) => {
+      state.empList.splice(action.payload, 1);
+    },
+
     setEditEmployeeData: (
       state,
       action: { payload: Employee; type: string }
     ) => {
       state.editEmployeeData = action.payload;
     },
+
     resetEditEmployeeData: (state) => {
       state.editEmployeeData = {};
-    },
+    }
   },
 });
 
